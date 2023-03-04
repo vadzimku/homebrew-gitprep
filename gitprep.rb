0cc3be814b21fcdf403b974c65e622d5740c63ed
vadiku <76070733+vadiku@users.noreply.github.com>
2023-03-04 11:32:22 +0100

    Release v0.0.5

--- gitprep.rb
+++ gitprep.rb
@@ -1,8 +1,8 @@
 class Gitprep < Formula
   desc "Gitprep set up your git repository including adding a hook that extracts a ticket number from a branch and adds it to your commits"
   homepage "https://github.com/vadiku/gitprep"
-  url "https://github.com/vadiku/gitprep/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "671f83ef498e2ad11cf9cf970fc4d78af3897bd6e95f675140edfb51f28dd9a2"
+  url "https://github.com/vadiku/gitprep/archive/refs/tags/v0.0.5.tar.gz"
+  sha256 "4c827300ab93018794364b464e9edbbaab161a34f78279e87a4b1d75dd08239a"
   license "MIT"
 
   def install
